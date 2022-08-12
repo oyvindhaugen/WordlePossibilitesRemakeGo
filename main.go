973a2e73309c1206939b910cc519566d2982ff18
oyvindhaugen <oyvindet@gmail.com>
2022-08-12 21:50:17 +0200

    doesnt remove from list at all

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
-var words []string
-
 func main() {
+	var words []string
 	list, err := http.Get("https://gist.githubusercontent.com/cfreshman/a7b776506c73284511034e63af1017ee/raw/845966807347a7b857d53294525263408be967ce/wordle-nyt-answers-alphabetical.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -27,29 +27,46 @@ func main() {
 	for scanner.Scan() {
 		words = append(words, scanner.Text())
 	}
+	fmt.Println(len(words))
+	letter := "a"
+	letterUpper := strings.ToUpper(letter)
+	x := 0
 	for {
-		fmt.Println("Which letter do you wish to remove?")
-		letter := ""
-		fmt.Scanln(&letter)
-		if letter != "wq" {
-			for i, wn := range words {
-				if strings.Contains(wn, letter) {
-					words = remove(words, i)
-				}
-			}
-			fmt.Println(len(words))
-		} else {
-			for _, word := range words {
-				fmt.Println(word)
-			}
-			fmt.Printf("You had %v possibilities left.", len(words))
+		fmt.Scan(&letter)
+		if letter == "wq" {
 			break
 		}
+		for i := 0; i < len(words); i++ {
+			if strings.Contains(words[i], letter) || strings.Contains(words[i], letterUpper) {
+				copy(words[i:], words[i+1:])
+				words[len(words)-1] = "no"
+				words = words[:len(words)-1]
+			}
+		}
+		fmt.Printf("Removed all words with %s\n", letter)
+	}
+	for num, _ := range words {
+		if words[num] == "no" {
+			x++
+		}
+	}
+	fmt.Printf("There are %v elements left\n", len(words[:len(words)-x]))
+	fmt.Printf("Do you want to print it out? y/n\n")
+	ans := ""
+	fmt.Scan(&ans)
+	if ans == "y" {
+		for _, word := range words {
+			if word != "no" {
+				fmt.Println(word)
+			}
+		}
+	} else {
+		os.Exit(0)
 	}
 }
-func remove(s []string, i int) []string {
-	copy(s[i:], s[i+1:])
-	s[len(s)-1] = ""
-	s = s[:len(s)-1]
-	return s
+func remove(slice []string, s int) []string {
+	copy(slice[s:], slice[s+1:])
+	slice[len(slice)-1] = "no"
+	slice = slice[:len(slice)-1]
+	return slice
 }
